a900e2c3866933a98bfd8268279eb56a0b022f0c
Vincent Biancarelli <Vincentgbm@gmail.com>
2023-10-23 13:04:22 -0400

    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(minNum, maxNum) {
 
+    let finalNumber = 0
+
+    if (minNum < 0 || maxNum < 0) {
+        return "ERROR"
+    } else if (typeof minNum != "number" || typeof maxNum != "number") {
+        return "ERROR"
+    }
+
+    if (maxNum < minNum) {
+        [minNum, maxNum] = [maxNum, minNum]
+    }
+
+    for (i = minNum; i < maxNum + 1; i++) {
+        finalNumber += i
+    }
+
+    return finalNumber;
 };
 
 // Do not edit below this line
